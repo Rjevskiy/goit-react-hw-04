922654bcf540641cf55685d1664a5882ff0aa85d
Alexsander Rjevskiy <porezkakir2@gmail.com>
2024-12-29 12:34:25 +0200

    Update ErrorMessage.jsx

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function ErrorMessage({ message }) {
   return (
     <div style={{ color: 'red', fontSize: '38px', fontWeight: 'bold' }}>
-          {`Схоже сталася якась помилка. ${message}`}
+          {`Схоже сталася якась помилка.`}
     </div>
   );
 }
